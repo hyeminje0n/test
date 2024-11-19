3a11a37a8de358b4d93dea3d1c14c6cb78257b62
hyeminje0n <hyemin11@k-hack.co.jp>
2024-11-19 15:41:40 +0900

    複数スライド実装

--- yoko.js
+++ yoko.js
@@ -1,19 +1,22 @@
 gsap.registerPlugin(ScrollTrigger);
-const container = document.querySelector(".scroll-container");
+const container = document.querySelectorAll(".scroll-container");
 
 if (container) {
-	const inner = document.querySelector(".scroll-list");
-	gsap.to(inner, {
-		x: () => -(inner.scrollWidth - document.documentElement.clientWidth) + "px",
-		ease: "none",
-		scrollTrigger: {
-			start: "top",
-			trigger: container,
-			duration: 10,
-			pin: true,
-			scrub: 1,
-			invalidateOnRefresh: true,
-			end: () => `+=${inner.offsetWidth}`,
-		},
+	container.forEach((wrapper) => {
+		const inner = wrapper.querySelector(".scroll-list");
+		gsap.to(inner, {
+			x: () =>
+				-(inner.scrollWidth - document.documentElement.clientWidth) + "px",
+			ease: "none",
+			scrollTrigger: {
+				start: "top",
+				trigger: wrapper,
+				duration: 10,
+				pin: true,
+				scrub: 1,
+				invalidateOnRefresh: true,
+				end: () => `+=${inner.offsetWidth}`,
+			},
+		});
 	});
 }
