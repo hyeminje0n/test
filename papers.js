980f3acb2750aa3d275e12eceefe113948e2f4b2
hyeminje0n <hyemin11@k-hack.co.jp>
2024-11-20 14:51:23 +0900

    書類機能テスト用

---
+++ papers.js
@@ -0,0 +1,38 @@
+document.addEventListener("DOMContentLoaded", function () {
+	const $papers = document.querySelectorAll(".paper");
+	$papers.forEach((paper) => {
+		let isClicked = false;
+		const hoverTl = gsap.timeline({ paused: true });
+		const clickTl = gsap.timeline({ paused: true });
+		const hoverAnimation = hoverTl.to(paper, {
+			y: -30,
+		});
+		const clickAnimation = clickTl.to(paper, {
+			top: 0,
+			width: "100vw",
+			height: "105vh",
+			zIndex: 10000,
+		});
+
+		paper.addEventListener("mouseover", () => {
+			if (!isClicked) {
+				hoverAnimation.play();
+			}
+		});
+
+		paper.addEventListener("mouseleave", () => {
+			if (!isClicked) {
+				hoverAnimation.reverse();
+			}
+		});
+
+		paper.addEventListener("click", () => {
+			isClicked = !isClicked;
+			if (isClicked) {
+				clickAnimation.play();
+			} else {
+				clickAnimation.reverse();
+			}
+		});
+	});
+});
