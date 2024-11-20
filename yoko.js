cb9154d39ee055713554253b625278dc886ca27b
hyeminje0n <hyemin11@k-hack.co.jp>
2024-11-20 13:20:48 +0900

    gsap가 로드된 후에 실행되도록 추가

--- yoko.js
+++ yoko.js
@@ -1,22 +1,30 @@
-gsap.registerPlugin(ScrollTrigger);
-const container = document.querySelectorAll(".scroll-container");
+document.addEventListener("DOMContentLoaded", function () {
+	// GSAP 및 ScrollTrigger가 정상적으로 로드된 후 실행
+	if (typeof gsap !== "undefined" && typeof ScrollTrigger !== "undefined") {
+		gsap.registerPlugin(ScrollTrigger);
 
-if (container) {
-	container.forEach((wrapper) => {
-		const inner = wrapper.querySelector(".scroll-list");
-		gsap.to(inner, {
-			x: () =>
-				-(inner.scrollWidth - document.documentElement.clientWidth) + "px",
-			ease: "none",
-			scrollTrigger: {
-				start: "top",
-				trigger: wrapper,
-				duration: 10,
-				pin: true,
-				scrub: 1,
-				invalidateOnRefresh: true,
-				end: () => `+=${inner.offsetWidth}`,
-			},
-		});
-	});
-}
+		const $scrollContainer = document.querySelectorAll(".scroll-container");
+
+		if ($scrollContainer) {
+			$scrollContainer.forEach((wrapper) => {
+				const inner = wrapper.querySelector(".scroll-list");
+				gsap.to(inner, {
+					x: () =>
+						-(inner.scrollWidth - document.documentElement.clientWidth) + "px",
+					ease: "none",
+					scrollTrigger: {
+						start: "top",
+						trigger: wrapper,
+						duration: 10,
+						pin: true,
+						scrub: 1,
+						invalidateOnRefresh: true,
+						end: () => `+=${inner.offsetWidth}`,
+					},
+				});
+			});
+		}
+	} else {
+		console.error("GSAP or ScrollTrigger is not loaded");
+	}
+});
